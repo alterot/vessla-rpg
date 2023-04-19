68415b8601d25c2cb4f91f5ebb94e5babf5f80e5
Max Alterot <101337822+alterot@users.noreply.github.com>
2023-04-19 23:09:57 +0200

    Add filter by spell level functionality

--- javascript.js
+++ javascript.js
@@ -1,30 +1,29 @@
-const spells = [ //array with dummy spell objects to test code - will fetch from back-end later. 
+const spells = [
     {
-        name: 'Shocking Grasp',
-        level: 0,
-        description: 'Lightning springs from your hand to deliver a shock to a creature you try to touch.',
+      name: 'Shocking Grasp',
+      level: 0,
+      description: 'Lightning springs from your hand to deliver a shock to a creature you try to touch.',
     },
     {
-        name: 'Chill Touch',
-        level: 0,
-        description: 'You create a ghostly, skeletal hand in the space of a creature within range.',
-
+      name: 'Chill Touch',
+      level: 0,
+      description: 'You create a ghostly, skeletal hand in the space of a creature within range.',
     },
     {
-        name: 'Detect Magic',
-        level: 1,
-        description: 'For the duration, you sense the presence of magic within 30 feet of you.',
+      name: 'Detect Magic',
+      level: 1,
+      description: 'For the duration, you sense the presence of magic within 30 feet of you.',
     },
     {
-        name: 'Misty Step',
-        level: 2,
-        description: 'Briefly surrounded by silvery mist, you teleport up to 30 feet to an unoccupied space that you can see.',
+      name: 'Misty Step',
+      level: 2,
+      description: 'Briefly surrounded by silvery mist, you teleport up to 30 feet to an unoccupied space that you can see.',
     },
   ];
-
-const spellList = document.querySelector('.spell-list ul');
-
-function addSpell(spell) {
+  
+  const spellList = document.querySelector('.spell-list ul');
+  
+  function addSpell(spell) {
     const li = document.createElement('li');
     li.id = spell.name; 
   
@@ -43,8 +42,26 @@ function addSpell(spell) {
     spellList.appendChild(li);
   }
   
-  
-
-spells.forEach(spell => {
+  spells.forEach(spell => {
     addSpell(spell);
-});
+  });
+  
+  const levelFilter = document.querySelector('#level-filter');
+  levelFilter.addEventListener('change', filterSpellsByLevel);
+  
+  function filterSpellsByLevel() {
+    const selectedLevel = this.value; // Get the selected value from the dropdown menu
+    const spellItems = document.querySelectorAll('.spell-list li'); // Get all the spell list items
+  
+    spellItems.forEach((item) => {
+      const spellLevel = item.querySelector('div:nth-child(2)').textContent.slice(-1); // Get the spell level from the second div in the list item
+      
+      // Check if the selected level matches the spell's level, or if "All" is selected
+      if (selectedLevel === '' || selectedLevel === spellLevel) {
+        item.style.display = 'block'; // Show the spell list item
+      } else {
+        item.style.display = 'none'; // Hide the spell list item
+      }
+    });
+  }
+  
